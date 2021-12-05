5c761c08676ca3fc2b3cadd66342e2cf1dcf5a28
AndrewKahr <22359829+AndrewKahr@users.noreply.github.com>
2021-12-04 19:53:28 -0800

    Update run.py

--- run.py
+++ run.py
@@ -5,6 +5,6 @@ import subprocess
 cmd = ["C:/Program Files/Unity Hub/Unity Hub.exe", '--', '--headless',  'install', '--version',  '2020.3.24f1',  '--changeset',  '79c78de19888']
 subprocess.call(cmd)
 
-while(not os.path.exists(r"C:\Program Files\Unity\Hub\Editor\2020.3.6f1\Editor\Unity.exe")):
+while(not os.path.exists(r"C:\Program Files\Unity\Hub\Editor\2020.3.24f1\Editor\Unity.exe")):
   print(os.system("tasklist"))
   time.sleep(10)
