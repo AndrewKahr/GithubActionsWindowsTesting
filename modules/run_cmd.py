86178ce311ba404e1afa4b7bdadfab92b1166062
AndrewKahr <22359829+AndrewKahr@users.noreply.github.com>
2021-12-05 01:36:37 -0800

    Update run_cmd.py

--- modules/run_cmd.py
+++ modules/run_cmd.py
@@ -6,3 +6,4 @@ def run_cmd(cmd):
     with Popen(cmd, stdout=PIPE, bufsize=1, universal_newlines=True) as p:
         for line in p.stdout:
             print(line, end='')  # process line here
+        return p.returncode
