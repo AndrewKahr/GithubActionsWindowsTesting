6bda7fe2cc185f976284d114cacd184d582e87d3
AndrewKahr <22359829+AndrewKahr@users.noreply.github.com>
2021-12-04 20:18:19 -0800

    Update run.py

--- run.py
+++ run.py
@@ -2,12 +2,6 @@ import os
 import time
 from subprocess import Popen, PIPE, CalledProcessError
 
-cmd = ["C:/Program Files/Unity Hub/Unity Hub.exe", '--', '--headless',  'install', '--version',  '2020.3.24f1',  '--changeset',  '79c78de19888']
-with Popen(cmd, stdout=PIPE, bufsize=1, universal_newlines=True) as p:
-    for line in p.stdout:
-        print(line, end='') # process line here
-
-list_files(r'C:\Program Files\Unity\Hub\Editor')
 
 def list_files(startpath):
   for root, dirs, files in os.walk(startpath):
@@ -17,3 +11,13 @@ def list_files(startpath):
       subindent = ' ' * 4 * (level + 1)
       for f in files:
           print('{}{}'.format(subindent, f))
+
+
+cmd = ["C:/Program Files/Unity Hub/Unity Hub.exe", '--', '--headless',  'install', '--version',  '2020.3.24f1',  '--changeset',  '79c78de19888']
+with Popen(cmd, stdout=PIPE, bufsize=1, universal_newlines=True) as p:
+    for line in p.stdout:
+        print(line, end='') # process line here
+
+list_files(r'C:\Program Files\Unity\Hub\Editor')
+
+
