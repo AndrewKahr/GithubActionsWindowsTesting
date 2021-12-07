ae839046607d73ee3c2fbcd4840808b5e140c109
AndrewKahr <22359829+AndrewKahr@users.noreply.github.com>
2021-12-07 01:24:43 -0800

    Update activate_unity.py

--- modules/activate_unity.py
+++ modules/activate_unity.py
@@ -1,7 +1,8 @@
+import os
 from modules.run_cmd import run_cmd
 
-activate_cmd = [r"C:\Program Files\Unity\Hub\Editor\2020.3.24f1\Editor\Unity.exe", '-batchmode', '-quit', '-nographics',
-                '-manualLicenseFile',  'license.ulf', '-logfile']
+activate_cmd = [r"C:\Program Files\Unity\Hub\Editor\2020.3.24f1\Editor\Unity.exe", '-batchmode', '-username', os.getenv('UNITY_USER'), 
+                '-password', os.getenv('UNITY_PASS'), '-serial', os.getenv('UNITY_SERIAL'), '-quit', '-nographics', '-logfile']
 
 
 def activate_unity():
